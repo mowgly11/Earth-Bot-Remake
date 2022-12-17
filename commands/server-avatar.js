a1163bdd488c0387c2ea04005405b8f312b12f13
mowgly11 <83482871+mowgly11@users.noreply.github.com>
2022-12-17 09:21:45 +0100

    server avatar & server banner

---
+++ commands/server-avatar.js
@@ -0,0 +1,33 @@
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("server-avatar")
+    .setDescription("display the current server's icon"),
+
+
+  async execute(client, interaction) {
+    await interaction.deferReply();
+
+    const replies = [
+      'my rate : ⭐⭐⭐⭐⭐',
+      'my rate : ⭐⭐⭐⭐'
+    ]
+
+    let embed = new EmbedBuilder()
+      .setAuthor({ name: `${interaction.guild.name}'s Icon` })
+      .setColor(0x0099FF)
+      .setTitle('Download Link')
+      .setURL(interaction.guild.iconURL({
+        dynamic: true,
+        size: 4096
+      }))
+      .setImage(interaction.guild.iconURL({ dynamic: true, size: 4096 }))
+      .setTimestamp()
+      .setFooter({ text: (replies[Math.floor(Math.random() * replies.length)]) })
+
+    await interaction.editReply({
+      embeds: [embed]
+    });
+  }
+}
---
+++ commands/server-banner.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("server-banner")
+        .setDescription("display the current server's banner"),
+
+    async execute(client, interaction) {
+        await interaction.deferReply();
+
+        const banner = interaction.guild.bannerURL({
+            dynamic: true,
+            size: 4096
+        });
+
+        if (!banner) return interaction.editReply({
+            content: "**This server has no banner.**",
+            ephemeral: true
+        });
+
+        await interaction.editReply({
+            content: ":arrow_right: Here is the server banner, i really like it",
+            files: [banner]
+        });
+    }
+}
